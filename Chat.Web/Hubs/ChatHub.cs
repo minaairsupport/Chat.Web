8e179c9cc1d56ca89a653676c0bbcb654521b458
Mina <mina.fawzy@sbdinc.com>
2022-12-15 15:44:19 +0200

    first commit

---
+++ Chat.Web/Hubs/ChatHub.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Chat.Web.Hubs
+{
+    public class ChatHub: Hub
+    {
+        public ChatHub()
+        {
+        }
+
+        public void Send(string user, string message)
+        {
+            Clients.Others.SendAsync("Receive", user, message);
+        }
+    }
+}
